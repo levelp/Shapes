e08aa33441171aa391aef8ed66734a414bf46422
miller777 <miller777@mail.ru>
2014-11-14 17:10:10 +0300

    Не понимаю.

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -9,5 +9,9 @@ public class Triangle extends Shape {
     @Override
     void show() {
         // TODO: показать
+        System.out.println("name = " + name);
+        System.out.println("x1= " + x1);
+        System.out.println("x2= " + x2);
+        System.out.println("x3= " + x3);
     }
 }
